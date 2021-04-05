7b40a887b7d2bd49807a8fff395f80443da2e3d2
Yuriy Prosvirnin <prosvirniny@gmail.com>
2021-04-05 22:53:09 +0300

    Initial Commit

---
+++ ApplePie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplePie
+//
+//  Created by YURY PROSVIRNIN on 05.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
